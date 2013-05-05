87634ab288fec09158fa3721d54ddd0836b3dcac
Kristen <kajudd@gmail.com>
2013-05-05 12:39:56 -0700

    tidy up login and signup forms

--- forms.py
+++ forms.py
@@ -1,13 +1,11 @@
-# from flask import Flask
-# from wtforms import Form, BooleanField, TextField, PasswordField, IntegerField, validators
 from flask_wtf import Form, TextField, BooleanField, PasswordField
 from flask_wtf import Required, validators
 
 class RegistrationForm(Form):
-    name = TextField('name', [validators.Length(min=1, max=25)])
-    email = TextField('email', [validators.Length(min=6, max=35)])
-    password = PasswordField('password', [Required()])
+    name = TextField('Name', [validators.Length(min=1, max=25)])
+    email = TextField('Email', [validators.Length(min=6, max=35)])
+    password = PasswordField('Password', [Required()])
 
 class LoginForm(Form):
-	email = TextField('email', [validators.Length(min=6, max=35)])
-	password = PasswordField('password', [Required()])
+	email = TextField('Email', [validators.Length(min=6, max=35)])
+	password = PasswordField('Password', [Required()])
